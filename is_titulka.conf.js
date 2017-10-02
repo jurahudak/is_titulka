461a4f9f485d86771750764dc3c2938237188b8d
Juraj Hudak <xhudak@fi.muni.cz>
2017-10-02 13:40:07 +0200

    verze 2 - konfigurace v externim souboru

---
+++ is_titulka.conf.js
@@ -0,0 +1,28 @@
+ //
+// Nastaveni pro is_titulka.js
+//
+
+var config = {
+  'tiles_my_order': {
+    "cs":['Vývoj','Systém','Lidé',
+          'Soubory','Pošta','Nástroje',
+          'Publikace','Karty','Studijní',
+         ],
+    "en":['Development','System','People',
+          'Files','My Mail','Tools',
+          'Publications','Cards','Studies',
+         ],
+    "sk":['Vývoj','Systém','Ľudia',
+          'Soubory','Pošta','Nástroje',
+          'Publikácie','Karty','Študijné',
+         ],
+  },
+  'tiles_delete': true,
+  'lookup_dril': true,
+  'lookup_discussion': true,
+  'have_no_life': false,
+  'tile_long_life': true,
+  'have_week_number': true,
+};
+
+// vim: tabstop=2 shiftwidth=2 softtabstop=2 expandtab
--- is_titulka.js
+++ is_titulka.js
@@ -2,6 +2,7 @@
 // @name        is_titulka
 // @namespace   is_titulka
 // @include     https://is.*.cz/auth/
+// @include     https://is.*.cz/auth/*
 // @include     https://is.*.cz/auth/?*
 // @version     1
 // @grant       none
@@ -9,49 +10,60 @@
 
 /*
    Sada funkcí, které trochu zlepší chování/vzhled/pořadí titulky IS (MU...) v novém designu.
-
    * změna pořadí dlaždic
    * mazání/skrytí zbývajících dlaždic
    * zobrazení oblastí drilu a počtu slovíček ke drilování
    * zkrácení života na délku dlaždic
-   * zobrazeni diskusí s nenulovým počtem nových příspěvků
-   * zobrazit číslo týdne mezi datem a časem v patičce
+   * zobrazení diskusí s nenulovým počtem nových příspěvků
+   * zobrazení čísla týdne mezi datem a časem v patičce
 */
 
-// zde uvést nadpisy dlaždic ve vytouženém pořadí
-var tiles_my_order =
-{
-    "cs":['Vývoj','Systém','Lidé',
+var config = {
+  // externí nastavení - odkomentovat, uvést vlastní url a zakomentovat stat. nastavení
+  'config_url': 'https://is.muni.cz/auth/www/60505/is_titulka.conf.js?Aab',
+  // statické nastavení - odkomentovat, uvést nstavení a zakomentovat url nastavení
+/*
+    // zde uvést nadpisy dlaždic ve vytouženém pořadí pro používaný jazyk
+    'tiles_my_order': {
+      "cs":['Vývoj','Systém','Lidé',
           'Soubory','Pošta','Nástroje',
           'Publikace','Karty','Studijní',
          ],
-    "en":['Development','System','People',
+      "en":['Development','System','People',
           'Files','My Mail','Tools',
           'Publications','Cards','Studies',
          ],
-    "sk":['Vývoj','Systém','Ľudia',
+      "sk":['Vývoj','Systém','Ľudia',
           'Soubory','Pošta','Nástroje',
           'Publikácie','Karty','Študijné',
          ],
-}[is.session.get('lang')];
+     },
 
-// nastavit na true, pokud uživatel chce uplně vymazat ostatní dlaždice, ktere nejsou uvedeny v tiles_my_order
-var tiles_delete = true;
+    // nastavit na true, pokud uživatel chce uplně vymazat ostatní dlaždice, ktere nejsou uvedeny v tiles_my_order
+    'tiles_delete': true,
 
-// dohledávat oblasti drilu
-var lookup_dril = true;
+    // dohledávat oblasti drilu
+    'lookup_dril': true,
 
-// zobrazovat nove diskuse
-var lookup_discussion = true;
+    // zobrazovat nove diskuse
+    'lookup_discussion': true,
 
-// bez zivota (na MU)
-var have_no_life = false;
+    // bez zivota (na MU)
+    'have_no_life': false,
 
-// zivot stejne dlouhy jako dlazdice
-var tile_long_life = true;
+    // zivot stejne dlouhy jako dlazdice
+    'tile_long_life': true,
 
-// číslo týdne
-var have_week_number = true;
+    // číslo týdne
+    'have_week_number': true,
+*/
+};
+
+if ( config.config_url !== undefined ) {
+  $.get(config.config_url, {}, function(data) { eval(data); run_me(config); });
+} else {
+  run_me(config);
+}
 
 /* definice funkcí a jejich spouštění ... za tímto komentařem už asi nic nechcete měnit ;-) */
 
@@ -60,10 +72,12 @@ var _messages = {
   'cs' : {
           "TILE_MISSING": "Chybí nějaká dlaždice? Možná je potřeba <b>vypnout</b> tento užasný user-script pro titulku nebo <b>změnit</b> jeho nastavení. ;-)",
           "DISCUSSIONS": "Diskusní fóra",
+          "WEEK_OF_YEAR": "Kalendářní týden",
          },
   'en' : {
           "TILE_MISSING": "An tile is missing? Maybe it's necessary to <b>turn off</b> this awesome user-script for the Title page or <b>change</b> its settings. ;-)",
           "DISCUSSIONS": "Discussions groups",
+          "WEEK_OF_YEAR": "week of year",
          },
 };
 _messages['sk'] = {};
@@ -107,7 +121,7 @@ function dlazdice_reorder(mp, mazat) {
     }
   }
   // explicitně neuvedené dlaždice vymazat ze stránky
-  if ( tiles_delete ) {
+  if ( mazat ) {
     stavajici_poradi = $('.dlazdice .row .nazev a').map(function () {
         return $(this).text();
       });
@@ -117,11 +131,6 @@ function dlazdice_reorder(mp, mazat) {
       }
     }
   }
-}
-
-// přerovnat (a případně promazat) dlaždice
-if ( tiles_my_order !== undefined && tiles_my_order.length > 0 ) {
-  dlazdice_reorder(tiles_my_order, tiles_delete);
 }
 
 // ajax handler pro dril
@@ -141,87 +150,102 @@ function dril_ajax_count(o_nazev, o_id, o_href) {
     );
 }
 
-// pokud uzivatel chce zobrazovat oblasti drilu na titulce
-if ( lookup_dril ) {
-  // div pro dril
-  $('#dlazdice').prepend(
-    '<div class="row" style="padding-left: 2em;"><div class="column"><div class="dlazdice" style="text-align: left; "><div id="dril_dlazdice"><img src="/pics/design/pracuji.gif"></div></div></div></div>'
-  );
-
-  // zjistit oblasti drilu
-  $.get('/auth/dril/', {},
-      function(data) {
-        $('#dril_dlazdice').html('');
-        // vytahnout vsechny oblasti ke drilovani
-        var pole = data.match(/(href=".*oblast_id=\d+.*<b>.*?<\/b>)/g);
-        // pres vsechny oblasti zjistit nazev, id a pocet slovicek ke drilovani a vsechno vykreslit
-        for ( var i in pole ) {
-          var oblast_href = pole[i].match(/href="(.*)"/)[1];
-          var oblast_id = pole[i].match(/oblast_id=(\d+)/)[1];
-          var oblast_nazev = pole[i].match(/<b>(.*)<\/b>/)[1];
-          dril_ajax_count(oblast_nazev, oblast_id, oblast_href);
-        }
-      }
-  );
-}
-
-// pokud uzivatel chce zobrazit souhrn novych z diskusi
-if ( lookup_discussion ) {
-  // vlozit jako dlazdici
-/*  $('#dlazdice').prepend(
-    '<div class="row" style="padding-left: 2em;"><div class="column"><div class="dlazdice" style="text-align: left; overflow: auto;"><div id="diskuse_dlazdice"></div></div></div></div>'
-  );
-*/
-  // vlozit pred Zivot
-  $('.zivot_column').prepend('<div><div class="nazev" style="font-size: 1.5rem; margin-bottom: 1em;"><a href="/auth/diskuse/">'+_m('DISCUSSIONS')+'</a></div><div style="margin-bottom: 2em; height: 15em; overflow: auto;" id="diskuse_dlazdice"><img src="/pics/design/pracuji.gif"></div><hr /></div>');
-
-  // zjistit diskuse
-  $.get('/auth/diskuse/', {},
-      function(data) {
-        $('#diskuse_dlazdice').html('');
-        var pole = data.match(/<li>(.*?\d+.*?(:?nov|new mess).*?)<\/li>/ig);
-        for ( var i in pole ) {
-          var nazev = (pole[i].match(/<b>(.*?)<\/b>/i) || ['',''])[1];
-          var url = //'/auth'+(
-              (pole[i].match(/href="([^"]+)"/i) || ['',''])[1];//.match(/(\/diskuse\/.*/))[1];
-          url = url.replace('../','/auth/');
-          var pocet = parseInt( (pole[i].match(/<font[^>]*>.*?(\d+)/i) || ['',0])[1] );
-          // pouze ty diskuse s nenulovym poctem novych
-          if ( pocet > 0 ) {
-            $('#diskuse_dlazdice').append( '<a href="'+url+'">'+nazev+'</a> <b>('+pocet+')</b><br />' );
-          }
-        }
-        recount_life_height();
-      }
-  );
-}
-
-// disclaimer na závěr
-$('#dlazdice').append('<div class="row"><div class="column" style="text-align: center;">' + _messages[is.session.get('lang')]['TILE_MISSING'] + '</div></div>');
-
-// remove life :-)
-if ( have_no_life ) {
-  $('#zivot').remove();
-} else {
-  if ( tile_long_life ) {
-    // zkrátit
-    recount_life_height();
-    // scrollbary
-    $('#zivot').css( 'overflow', 'auto' );
-  }
-}
-
+// přepočítání výsky života
 function recount_life_height() {
   $('#zivot').height( $('#dlazdice').height() + $('#vyhledavani').height() - $('#diskuse_dlazdice').height() );
 }
 
-if ( have_week_number ) {
-  Date.prototype.getWeek = function() {
-    var onejan = new Date(this.getFullYear(),0,1);
-    return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
-  };
-  $('.footer_datum span').prepend('('+(new Date()).getWeek()+'.) ');
-}
 
+// hlavni funkce
+function run_me(cfg) {
+
+  // dlaždice dle jazyka
+  var tiles_my_order = cfg.tiles_my_order[is.session.get('lang')];
+
+  // přerovnat (a případně promazat) dlaždice
+  if ( tiles_my_order !== undefined && tiles_my_order.length > 0 ) {
+    dlazdice_reorder(tiles_my_order, cfg.tiles_delete);
+  }
+
+  // pokud uzivatel chce zobrazovat oblasti drilu na titulce
+  if ( cfg.lookup_dril ) {
+    // div pro dril
+    $('#dlazdice').prepend(
+      '<div class="row" style="padding-left: 2em;"><div class="column"><div class="dlazdice" style="text-align: left; "><div id="dril_dlazdice"><img src="/pics/design/pracuji.gif"></div></div></div></div>'
+    );
+
+    // zjistit oblasti drilu
+    $.get('/auth/dril/', {},
+        function(data) {
+          $('#dril_dlazdice').html('');
+          // vytahnout vsechny oblasti ke drilovani
+          var pole = data.match(/(href=".*oblast_id=\d+.*<b>.*?<\/b>)/g);
+          // pres vsechny oblasti zjistit nazev, id a pocet slovicek ke drilovani a vsechno vykreslit
+          for ( var i in pole ) {
+            var oblast_href = pole[i].match(/href="(.*)"/)[1];
+            var oblast_id = pole[i].match(/oblast_id=(\d+)/)[1];
+            var oblast_nazev = pole[i].match(/<b>(.*)<\/b>/)[1];
+            dril_ajax_count(oblast_nazev, oblast_id, oblast_href);
+          }
+        }
+    );
+  }
+
+  // pokud uzivatel chce zobrazit souhrn novych z diskusi
+  if ( cfg.lookup_discussion ) {
+    // vlozit jako dlazdici
+  /*  $('#dlazdice').prepend(
+      '<div class="row" style="padding-left: 2em;"><div class="column"><div class="dlazdice" style="text-align: left; overflow: auto;"><div id="diskuse_dlazdice"></div></div></div></div>'
+    );
+  */
+    // vlozit pred Zivot
+    $('.zivot_column').prepend('<div><div class="nazev" style="font-size: 1.5rem; margin-bottom: 1em;"><a href="/auth/diskuse/">'+_m('DISCUSSIONS')+'</a></div><div style="margin-bottom: 2em; height: 15em; overflow: auto;" id="diskuse_dlazdice"><img src="/pics/design/pracuji.gif"></div><hr /></div>');
+
+    // zjistit diskuse
+    $.get('/auth/diskuse/', {},
+        function(data) {
+          $('#diskuse_dlazdice').html('');
+          var pole = data.match(/<li>(.*?\d+.*?(:?nov|new mess).*?)<\/li>/ig);
+          for ( var i in pole ) {
+            var nazev = (pole[i].match(/<b>(.*?)<\/b>/i) || ['',''])[1];
+            var url = //'/auth'+(
+                (pole[i].match(/href="([^"]+)"/i) || ['',''])[1];//.match(/(\/diskuse\/.*/))[1];
+            url = url.replace('../','/auth/');
+            var pocet = parseInt( (pole[i].match(/<font[^>]*>.*?(\d+)/i) || ['',0])[1] );
+            // pouze ty diskuse s nenulovym poctem novych
+            if ( pocet > 0 ) {
+              $('#diskuse_dlazdice').append( '<a href="'+url+'">'+nazev+'</a> <b>('+pocet+')</b><br />' );
+            }
+          }
+          recount_life_height();
+        }
+    );
+  }
+
+  // disclaimer na závěr
+  $('#dlazdice').append('<div class="row"><div class="column" style="text-align: center;">' + _m('TILE_MISSING') + '</div></div>');
+
+  // remove life :-)
+  if ( cfg.have_no_life ) {
+    $('#zivot').remove();
+  } else {
+    if ( cfg.tile_long_life ) {
+      // zkrátit
+      recount_life_height();
+      // scrollbary
+      $('#zivot').css( 'overflow', 'auto' );
+    }
+  }
+
+  // číslo týdne v patičce
+  if ( cfg.have_week_number ) {
+    Date.prototype.getWeek = function() {
+      var onejan = new Date(this.getFullYear(),0,1);
+      return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
+    };
+    $('.footer_datum span').prepend('(<a href="/auth/rozvrh/sl_tyden" title="'+_m('WEEK_OF_YEAR')+'">'+(new Date()).getWeek()+'.</a>) ');
+  }
+
+} // run_me()
 
 // vim: tabstop=2 shiftwidth=2 softtabstop=2 expandtab
